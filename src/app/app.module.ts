d748f3382d574335da4551f2cf97bac19b21ef76
Specter <mikexx1264@gmail.com>
2021-06-04 01:58:41 -0500

    Initial configuration of the project with Home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { RoomComponent } from './pages/room/room.component';
+import { VideoPlayerComponent } from './components/video-player/video-player.component';
+import { MenuBottomComponent } from './components/menu-bottom/menu-bottom.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    RoomComponent,
+    VideoPlayerComponent,
+    MenuBottomComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/menu-bottom/menu-bottom.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-bottom',
+  templateUrl: './menu-bottom.component.html',
+  styleUrls: ['./menu-bottom.component.scss']
+})
+export class MenuBottomComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
